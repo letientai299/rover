f12779965b8f9407801c4e390f099e30cf4db020
Tai <letientai299@gmail.com>
2024-04-05 01:42:04 +0700

    Add Split component
    I've tried several other libs, but unhappy with all of them. Some
    doesn't work with Typescript, some is confusing. Also, this is
    complicated but not that hard to build myself. So I did it.

--- fe/src/components/atoms/Split/Split.tsx
+++ fe/src/components/atoms/Split/Split.tsx
@@ -1,16 +1,17 @@
 import { cx } from '@/utils';
-import {
-  CSSProperties,
+import React, {
   HTMLAttributes,
+  MouseEvent,
   PropsWithChildren,
-  ReactNode,
+  useCallback,
+  useLayoutEffect,
+  useRef,
 } from 'react';
 
 import styles from './split.module.scss';
 
 // TODO:
 //  - resizable using keyboard shortcuts
-//  - resizable using mouse drag
 //  - make Split remembers its children sizes on load/resume.
 //  - should we allow tab to focus the divider
 //  - should we add something like ctr-shift-r to start resizing mode (divider
@@ -18,42 +19,221 @@ import styles from './split.module.scss';
 
 type Direction = 'horizontal' | 'vertical';
 
-interface DividerProps extends HTMLAttributes<HTMLDivElement> {
-  direction: Direction;
-}
+const DIVIDER_ROLE = 'separator';
 
-const Divider = (props: DividerProps) => {
-  return (
-    <div
-      tabIndex={-1}
-      className={styles.divider}
-      data-direction={props.direction}
-    />
-  );
+type ResizingPair = {
+  a: HTMLElement;
+  aSize: number;
+  b: HTMLElement;
+  bSize: number;
 };
 
-function interleave<T>(all: T[] | T, x: T): T[] {
-  const arr = Array.isArray(all) ? all : [all];
-  return arr.flatMap((v) => [v, x]).splice(0, arr.length * 2 - 1);
-}
+type Point = {
+  X: number;
+  Y: number;
+};
 
 const Split = (props: SplitProps) => {
-  const { className, direction, children, ...rest } = props;
-  const isHorizontal = direction === 'horizontal';
-  const style: CSSProperties = {
-    flexDirection: isHorizontal ? 'row' : 'column',
-  };
+  const { style, className, direction, children, ...rest } = props;
+  const isVerticalDivider = direction === 'horizontal';
+  const ref = useRef<HTMLDivElement>(null);
+  const resizing = useRef<{ pair: ResizingPair | null }>({ pair: null });
+  const mousePos = useRef<{ pos: Point | null }>({ pos: null });
 
-  const divider = (
-    <Divider direction={isHorizontal ? 'vertical' : 'horizontal'} />
+  const getChildElements = useCallback(
+    (split: HTMLDivElement) =>
+      [...split.children].filter((el) => el.role !== DIVIDER_ROLE),
+    [],
   );
 
-  console.log(children);
-  const kids = interleave<ReactNode>(children, divider);
-  console.log(kids);
+  const isResizing = () => {
+    return resizing.current.pair && mousePos.current.pos;
+  };
+
+  // on resize start, convert all the percentage value of the flex grow
+  // in the inline style of all child elements into concrete size in pixels,
+  // so that irrelevant elements won't be resized.
+  const resizeStart = () => {
+    const split = ref.current;
+    if (!split) {
+      return;
+    }
+
+    getChildElements(split).forEach((e) => {
+      const elem = e as HTMLElement;
+      const computed = window.getComputedStyle(elem);
+      if (isVerticalDivider) {
+        elem.style.width = computed.width;
+      } else {
+        elem.style.height = computed.height;
+      }
+      elem.style.flexGrow = `0`; // clear inline width set by resize function
+    });
+  };
+
+  // during resizing, update the size of relevant pair of elements via
+  // inline style.
+  const resize = (e: MouseEvent) => {
+    if (!isResizing()) {
+      return;
+    }
+
+    const pair = resizing.current.pair!;
+    const pos = mousePos.current.pos!;
+    // compute delta between mouse move using screenX/Y because movementX/Y
+    // unit is browser specific.
+    // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/movementX
+    const moved = isVerticalDivider ? e.screenX - pos.X : e.screenY - pos.Y;
+
+    if (pair.aSize - moved < 0 || pair.bSize + moved < 0) {
+      return; // ignore invalid resize
+    }
+
+    mousePos.current.pos = { X: e.screenX, Y: e.screenY };
+
+    pair.aSize = pair.aSize - moved;
+    pair.bSize = pair.bSize + moved;
+
+    if (isVerticalDivider) {
+      pair.a.style.width = `${pair.aSize}px`;
+      pair.b.style.width = `${pair.bSize}px`;
+    } else {
+      pair.a.style.height = `${pair.aSize}px`;
+      pair.b.style.height = `${pair.bSize}px`;
+    }
+  };
+
+  // adjust all child elements inline style to use percentage value instead of
+  // fixed width/height, so that the flex layout will be responsive against
+  // browser window resizing.
+  const resizeEnd = () => {
+    const split = ref.current;
+    if (!split) {
+      return;
+    }
+
+    const total = totalSize(split, isVerticalDivider);
+    getChildElements(split).forEach((e) => {
+      const elem = e as HTMLElement;
+      const computed = window.getComputedStyle(elem);
+      const size = parseSize(
+        isVerticalDivider ? computed.width : computed.height,
+      );
+      const percent = (size * 100) / total;
+      elem.style.flexGrow = `${percent}`;
+      // clear inline width set by resize function
+      if (isVerticalDivider) {
+        elem.style.width = `0`;
+      } else {
+        elem.style.height = `0`;
+      }
+    });
+    // .reduce((a, all) => all + a, 0);
+
+    resizing.current.pair = null;
+    mousePos.current.pos = null;
+  };
+
+  const startDragging = (pos: Point, d: HTMLElement) => {
+    // guaranteed to be not null
+    const a = d.nextElementSibling! as HTMLElement;
+    // guaranteed to be not null
+    const b = d.previousElementSibling! as HTMLElement;
+    const aStyle = window.getComputedStyle(a);
+    const bStyle = window.getComputedStyle(b);
+
+    resizing.current.pair = {
+      a: a,
+      aSize: parseSize(isVerticalDivider ? aStyle.width : aStyle.height),
+      b: b,
+      bSize: parseSize(isVerticalDivider ? bStyle.width : bStyle.height),
+    };
+
+    mousePos.current.pos = pos;
+    resizeStart();
+  };
+
+  const initChildPercentage = useCallback(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    if (!Array.isArray(children)) {
+      return; // no need to init size when there's not many children.
+    }
+
+    const stats = getChildElements(ref.current).reduce(
+      (stats, re) => {
+        const e = re as HTMLElement;
+        const elemStyle = e.style;
+        if (elemStyle.flexGrow === '') {
+          stats.newElems.push(e);
+          return stats;
+        }
+
+        stats.currentGrowSum += parseFloat(elemStyle.flexGrow);
+        return stats;
+      },
+      // find all new elements that need to have reasonable initial flexGrow,
+      // and the sum of all current elements flexGrow.
+      { newElems: [] as HTMLElement[], currentGrowSum: 0 },
+    );
+
+    const grow =
+      children.length === stats.newElems.length
+        ? // if all elements are new, all will grow to be equal.
+          100 / children.length
+        : // otherwise, those new elements get an equal fraction of total sizes,
+          // which is based on the current total grow sum.
+          stats.currentGrowSum / (children.length - stats.newElems.length);
+
+    stats.newElems.forEach((elem) => {
+      elem.style.flexGrow = `${grow}`;
+      if (isVerticalDivider) {
+        elem.style.width = '0';
+      } else {
+        elem.style.height = '0';
+      }
+    });
+  }, [children, getChildElements, isVerticalDivider]);
+
+  useLayoutEffect(initChildPercentage, [initChildPercentage]);
+
+  const stopDragging = (e: MouseEvent<HTMLDivElement>) => {
+    if (e.button !== 0) {
+      return; // not the primary button (i.e. left for right-handed users)
+    }
+
+    e.stopPropagation();
+    e.preventDefault();
+    resizeEnd();
+  };
+
+  const mouseUp = (e: MouseEvent<HTMLDivElement>) => {
+    if (isResizing()) {
+      stopDragging(e);
+    }
+  };
+
+  const mouseLeave = (e: MouseEvent<HTMLDivElement>) => {
+    if (isResizing()) {
+      stopDragging(e);
+    }
+  };
+
+  const kids = addDividers(children, isVerticalDivider, startDragging);
 
   return (
-    <div style={style} {...rest} className={cx(styles.split, className)}>
+    <div
+      ref={ref}
+      style={style}
+      {...rest}
+      className={cx(styles.split, className)}
+      data-split-direction={direction}
+      onMouseUp={mouseUp}
+      onMouseLeave={mouseLeave}
+      onMouseMove={resize}
+    >
       {kids}
     </div>
   );
@@ -62,5 +242,82 @@ const Split = (props: SplitProps) => {
 export type SplitProps = PropsWithChildren<HTMLAttributes<HTMLDivElement>> & {
   direction: Direction;
 };
+
+function addDividers(
+  all: React.ReactNode[] | React.ReactNode,
+  isVertical: boolean,
+  startDragging: (pos: Point, divider: HTMLElement) => void,
+): React.ReactNode[] {
+  const arr = Array.isArray(all) ? all : [all];
+  return (
+    arr
+      // ensure all non-divider children must be within an HTML element
+      // so that we can attach style on it later.
+      .map((v, i) => {
+        return ['function', 'object'].includes(typeof v) ? (
+          v
+        ) : (
+          <div key={`split-element-${i}`}>{v}</div>
+        );
+      })
+      // interleave with dividers
+      .flatMap((v, i) => [
+        v,
+        <Divider
+          direction={isVertical ? 'vertical' : 'horizontal'}
+          key={`split-divider-after-element-${i}`}
+          startDragging={startDragging}
+        />,
+      ])
+      // remove the last divider
+      .splice(0, arr.length * 2 - 1)
+  );
+}
+
+interface DividerProps extends HTMLAttributes<HTMLDivElement> {
+  direction: Direction;
+  startDragging: (pos: Point, divider: HTMLElement) => void;
+}
+
+const Divider = (props: DividerProps) => {
+  const { startDragging, direction, ...rest } = props;
+
+  const mouseDown = (e: MouseEvent<HTMLDivElement>) => {
+    // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button#value
+    // 0 means the primary button, i.e. left button for right-handed users.
+    if (e.button === 0) {
+      startDragging({ X: e.screenX, Y: e.screenY }, e.currentTarget);
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  };
+
+  return (
+    <div
+      // start dragging when clicking onto divider
+      onMouseDown={mouseDown}
+      tabIndex={0}
+      className={styles.divider}
+      data-direction={direction}
+      // set the role to follow ARIA Window Splitter Pattern,
+      // (https://www.w3.org/WAI/ARIA/apg/patterns/windowsplitter/)
+      // also use this as an indicator for Split to find non-divider children.
+      role={DIVIDER_ROLE}
+      {...rest}
+    >
+      <div className={styles.line} />
+    </div>
+  );
+};
+
+const totalSize = (
+  split: HTMLDivElement,
+  isVerticalDivider: boolean,
+): number => {
+  const computed = window.getComputedStyle(split);
+  return parseSize(isVerticalDivider ? computed.width : computed.height);
+};
+
+const parseSize = (s: string) => parseFloat(s.replace('px', ''));
 
 export default Split;
---
+++ fe/src/components/atoms/Split/tests/SplitDemo.tsx
@@ -0,0 +1,111 @@
+import { Button } from '@/components/atoms';
+import { HTMLAttributes, useRef, useState } from 'react';
+import { FiPlusSquare } from 'react-icons/fi';
+import Split from '../Split';
+
+interface BoxProps extends HTMLAttributes<HTMLDivElement> {}
+
+export function Box(props: BoxProps) {
+  const { style, children, ...rest } = props;
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        minHeight: `var(--sizes-9)`,
+        border: `1px solid var(--colors-bd-clear)`,
+        ...style,
+      }}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
+
+export function Vert() {
+  return (
+    <Box>
+      <Split direction={'vertical'}>
+        <div>Pane 1</div>
+        <div>Pane 2</div>
+        <div>Pane 3</div>
+      </Split>
+    </Box>
+  );
+}
+
+export function Hor() {
+  return (
+    <Box>
+      <Split direction={'horizontal'}>
+        <div>Pane 1</div>
+        <div>Pane 2</div>
+        <div>Pane 3</div>
+      </Split>
+    </Box>
+  );
+}
+
+export function Nested() {
+  return (
+    <Box>
+      <Split direction={'horizontal'}>
+        <div>Pane 1</div>
+        <Split direction={'vertical'} style={{}}>
+          <div>Pane 1</div>
+          <Split direction={'horizontal'}>
+            <div>Pane 1</div>
+            <div>Pane 2</div>
+          </Split>
+          <div>Pane 3</div>
+        </Split>
+        <div>Pane 2</div>
+      </Split>
+    </Box>
+  );
+}
+
+export function AddRemove() {
+  const paneID = useRef(3);
+  const [panes, setPanes] = useState([1, 2]);
+  const add = () =>
+    setPanes((v) => {
+      const res = [...v, paneID.current];
+      paneID.current++;
+      return res;
+    });
+
+  const remove = (id: number) => setPanes((v) => v.filter((x) => x !== id));
+
+  const children = panes.map((i) => {
+    return (
+      <div
+        key={i}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minWidth: 'fit-content',
+        }}
+      >
+        <Button onClick={() => remove(i)} variant={'outline'}>
+          Remove {i}
+        </Button>
+      </div>
+    );
+  });
+
+  return (
+    <Box style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ borderBottom: '1px solid var(--colors-bd-clear)' }}>
+        <Button onClick={add} icon={FiPlusSquare} variant={'outline'}>
+          Add new pane
+        </Button>
+      </div>
+      <Split direction={'horizontal'} style={{ flexGrow: 1 }}>
+        {children}
+      </Split>
+    </Box>
+  );
+}
