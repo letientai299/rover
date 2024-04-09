5dccf786be82a0fba7729adc3528adf7eee840d5
Tai <letientai299@gmail.com>
2024-04-09 23:56:57 +0700

    Simplify the Activity pane code

--- fe/src/design/mocks/Activity.tsx
+++ fe/src/design/mocks/Activity.tsx
@@ -1,41 +1,48 @@
-import { Button } from 'src/components';
-import Accordion from 'src/components/atoms/Accordion/Accordion.tsx';
-import { FaComputer, FaDownload } from 'react-icons/fa6';
+import {
+  FaCommentDots,
+  FaComputer,
+  FaDownload,
+  FaUser,
+  FaWrench,
+} from 'react-icons/fa6';
 import { HiDocument } from 'react-icons/hi2';
-import { MdMore, MdWork } from 'react-icons/md';
+import { Button } from 'src/components';
+import { Icon } from 'src/components/atoms';
+import Accordion from 'src/components/atoms/Accordion/Accordion.tsx';
 
 import styles from './Activity.module.scss';
 
 export function Activity() {
-  const itemStyle = { marginLeft: '0.5em' };
-  const more = new Array(2).fill(0).map((_, i) => (
-    <Button key={i} icon={MdMore} style={itemStyle}>
-      More button - {i + 1}
-    </Button>
-  ));
+  const more = new Array(2)
+    .fill(0)
+    .map((_, i) => (
+      <Bookmarked title={`More button - ${i}`} icon={FaCommentDots} />
+    ));
 
   return (
     <Accordion.Root className={styles.activity} open={true}>
       <Accordion.Header className={styles.activityTitle}>
         Favorites
       </Accordion.Header>
-      <Accordion.Body>
-        <Button icon={FaComputer} style={itemStyle}>
-          My PC
-        </Button>
-        <Button icon={HiDocument} style={itemStyle}>
-          Documents
-        </Button>
-        <Button icon={FaDownload} style={itemStyle}>
-          Downloads
-        </Button>
-        <Button icon={MdWork} style={itemStyle}>
-          Projects
-        </Button>
+      <Accordion.Body className={styles.activityBody}>
+        <Bookmarked icon={FaComputer} title={'My PC'} />
+        <Bookmarked icon={HiDocument} title={'Documents'} />
+        <Bookmarked icon={FaDownload} title={'Download'} />
+        <Bookmarked icon={FaUser} title={'Tai'} />
+        <Bookmarked icon={FaWrench} title={'Developers'} />
         {more}
       </Accordion.Body>
     </Accordion.Root>
   );
+}
+
+interface BookmarkedProps {
+  title: string;
+  icon: Icon;
+}
+
+function Bookmarked({ title, icon }: BookmarkedProps) {
+  return <Button icon={icon}>{title}</Button>;
 }
 
 export default Activity;
