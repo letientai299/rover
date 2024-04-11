faa75ec47a25e8b3bc55ff74deafc396b2df07dd
letientai299 <letientai299@users.noreply.github.com>
2024-04-11 08:31:31 +0000

    deploy: 8d3b1a9bfced72a3b16891c2406e62135f119960

--- assets/readme-BqBntMR7.js
+++
@@ -1,4 +0,0 @@
-import{j as n}from"./app-wXXRkJhG.js";function s(t){const e={a:"a",h1:"h1",li:"li",ul:"ul",...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h1,{children:"Design system"}),`
-`,n.jsxs(e.ul,{children:[`
-`,n.jsx(e.li,{children:n.jsx(e.a,{href:"#/design/colors.mdx",children:"Colors"})}),`
-`]})]})}function r(t={}){const{wrapper:e}=t.components||{};return e?n.jsx(e,{...t,children:n.jsx(s,{...t})}):s(t)}export{r as default};
